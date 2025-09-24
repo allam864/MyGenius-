9e9b56b8a1664520e8b64048d2d6650d6067d0ef
allam864 <48215395+allam864@users.noreply.github.com>
2025-09-24 06:04:01 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,14 +3,13 @@ require("dotenv").config();
 
 const app = express();
 
-// Route رئيسية للتجربة
+// Route للتجربة
 app.get("/", (req, res) => {
   res.send("✅ Server is working fine!");
 });
 
-// لازم ياخد البورت من Railway
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(🚀 Server running on port ${PORT});
 });
